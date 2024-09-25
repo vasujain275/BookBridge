59ab7056c06730b0e9f28014b0d8dcfe80c5a4b0
vasujain275 <vasujain275@gmail.com>
2024-09-25 22:29:49 +0530

    feat: basic spring boot boilerplate added

---
+++ api/src/main/java/me/vasujain/bookbridge/BookBridgeApplication.java
@@ -0,0 +1,13 @@
+package me.vasujain.bookbridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookBridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookBridgeApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/me/vasujain/bookbridge/BookBridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.vasujain.bookbridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookBridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
